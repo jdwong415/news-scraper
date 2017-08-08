05c794c6dd2bae32ac59f27ae615ca9e1f6072a4
Jonathan Wong <jdwong2@yahoo.com>
2017-08-08 00:40:37 -0700

    Return results after scrape

--- controllers/routes.js
+++ controllers/routes.js
@@ -12,7 +12,10 @@ router.get('/', function(req, res) {
 router.get('/scrape', function(req, res) {
   request("http://www.nytimes.com/section/technology", function(error, response, body) {
     var $ = cheerio.load(body);
-    
+    var arr = [];
+    var articles = [];
+    var count = 0;
+
     $(".story").each(function(i, element) {
       var heading = $(element).find("h2").text().trim();
       var url = $(element).find("a").attr("href");
@@ -22,15 +25,21 @@ router.get('/scrape', function(req, res) {
         url: url
       });
 
+      articles.push(newArticle);
+
       var promise = Article.find({ heading: heading, url: url }).exec();
       promise.then(function(res) {
         if (res.length === 0) {
           newArticle.save(function(err) {
+            count++;
             console.log("Article stored");
           });
         }
       });
     });
+    arr.push({ articles: articles });
+    arr.push({ count: count });
+    res.json(arr);
   });
 });
 
