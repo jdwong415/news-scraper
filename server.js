f975d99dc1a4d1732a7eb1718be1873c8785403a
Jonathan Wong <jdwong2@yahoo.com>
2017-08-07 19:15:53 -0700

    Setup mongoose

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cheerio = require('cheerio');
+var request = require('request');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-mongoose.connect('');
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.static('public'));
+
+var databaseUri = "mongodb://localhost/scraper"
+if (process.env.MONGODB_URI) {
+  mongooes.connect(process.env.MONGODB_URI, {
+    useMongoClient: true
+  });
+}
+else {
+  mongoose.connect(databaseUri, {
+    useMongoClient: true
+  });
+}
+
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
